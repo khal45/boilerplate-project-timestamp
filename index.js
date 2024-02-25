d5cc7f3f8f373b5007c13cfb0b15c5e2bbbd62e9
khal45 <khalifaariko@gmail.com>
2024-02-25 01:38:03 +0100

    feat:finished_basic_project

--- index.js
+++ index.js
@@ -14,19 +14,41 @@ app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+// function to check if the date is invalid
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/:date", (req, res) => {
+  // re.params.date gets the date value in the url
+  let date = new Date(req.params.date);
+  // condition to handle if date entered is invalid. date entered was a string that was not parsed
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  // second instance of invalid date, date is totally not a date at all
+  if (isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 // Listen on port set in environment variable or default to 3000
-let listener = app.listen(process.env.PORT || 3000, function () {
+let listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
